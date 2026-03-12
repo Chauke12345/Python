9ee1118dc6f2419634b15def02e0c346d072f4fa
Eddie <edwardchauke@hotmail.com>
2026-03-12 15:58:05 +0200

    Chapter_3

---
+++ Chapter_3/Try Out/Try_Out page 36.py
@@ -0,0 +1,23 @@
+names = ["Hleko", "Rimitso", "Hletelo", "Simeko"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+names = ["Hleko", "Rimitso", "Hletelo", "Simeko"]
+
+print("Hello Hleko, how are you doing today?")
+print("Hello Rimitso, how are you doing today?")
+print("Hello Hletelo, how are you doing today?")
+print("Hello Simeko, how are you doing today?")
+
+
+WishList = ["car", "house", "yatches", "horse", "private jet"]
+
+# Print statements about each item in the wishlist
+print("I would like to own a " + WishList[0] + " Volvo.")
+print("I would like to own a " + WishList[1] + " with double garage.")
+print("I would like to own a " + WishList[2] + " yes")
+print("I would like to own a " + WishList[3] + " the fatest one.")
+print("I would like to own a " + WishList[4] + " the most expensive one.")
